28a7dc6a686757c82ca454367e3f9589839bb5c2
Justin Iven Müller <53189020+JustinIven@users.noreply.github.com>
2026-07-07 12:54:54 +0000

    Refactor username parsing logic into a new module

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
 import asyncio
 import logging
-import base64
-import re
-import uuid
 from email import message_from_bytes
 
 from custom import CustomController
@@ -11,6 +8,7 @@ from aiosmtpd.smtp import AuthResult
 import sslContext
 import azure_table
 from graph import GraphClient
+from parsing import parse_username
 from env import (
     LOG_LEVEL,
     TLS_SOURCE,
@@ -19,67 +17,12 @@ from env import (
     TLS_CERT_FILEPATH,
     TLS_KEY_FILEPATH,
     TLS_CIPHER_SUITE,
-    USERNAME_DELIMITER,
     AZURE_KEY_VAULT_URL,
     AZURE_KEY_VAULT_CERT_NAME,
     AZURE_TABLES_FORCE_USAGE
 )
 
 
-
-
-def decode_uuid_or_base64url(input_str: str) -> str:
-    """
-    Checks if input is a UUID string, otherwise attempts to decode as base64url and convert to UUID string.
-    Returns a decoded string in UUID format.
-    """
-
-    # check if the input is a UUID
-    uuid_pattern = re.compile(r'^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$')
-    if uuid_pattern.match(input_str):
-        return input_str
-
-    # Attempt to decode as base64url
-    try:
-        return str(uuid.UUID(bytes=base64.urlsafe_b64decode(input_str + '=' * (-len(input_str) % 4))))
-    except Exception:
-        raise ValueError(f"Invalid base64url encoding in input '{input_str}'")
-
-
-
-def parse_username(username: str) -> tuple[str, str, str|None]:
-    """
-    Parse the username to extract tenant_id and client_id.
-    The expected format is: tenant_id{USERNAME_DELIMITER}client_id{. optional_tld}
-    """
-    
-    # remove the optional TLD if present
-    if '.' in username:
-        username = username.split('.')[0]
-
-    # Check if username is valid
-    if not username or USERNAME_DELIMITER not in username:
-        raise ValueError(f"Invalid username format. Expected format: tenant_id{USERNAME_DELIMITER}client_id")
-    
-    # Split the username by the delimiter
-    parts = username.split(USERNAME_DELIMITER)
-    if len(parts) != 2:
-        raise ValueError(f"Invalid username format. Expected exactly one '{USERNAME_DELIMITER}' character")
-    
-    # check if the second part hints a user stored in the lookup table
-    if parts[1] == 'lookup':
-        return azure_table.lookup_user(parts[0])
-
-    # else return both parts decoded
-    tenant_id = decode_uuid_or_base64url(parts[0])
-    client_id = decode_uuid_or_base64url(parts[1])
-
-    # If AZURE_TABLES_FORCE_USAGE is enabled, verify the user exists in the table
-    if AZURE_TABLES_FORCE_USAGE:
-        from_email = azure_table.verify_user_in_table(tenant_id, client_id)
-        return tenant_id, client_id, from_email
-
-    return tenant_id, client_id, None
 
 
 class Authenticator:
